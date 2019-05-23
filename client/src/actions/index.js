11437115b434d10cd6f6897d5791b68ee3197339
Adrian Wix <adrianwix18@gmail.com>
2019-05-23 18:32:47 +0200

    Section 4: Data Caching with Redis

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,33 +1,41 @@
-import axios from 'axios';
-import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from './types';
+import axios from "axios";
+import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from "./types";
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
+	const res = await axios
+		.get("/api/current_user")
+		.catch(err => console.log(err));
 
-  dispatch({ type: FETCH_USER, payload: res.data });
+	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
-  const res = await axios.post('/api/stripe', token);
+	const res = await axios
+		.post("/api/stripe", token)
+		.catch(err => console.log(err));
 
-  dispatch({ type: FETCH_USER, payload: res.data });
+	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitBlog = (values, history) => async dispatch => {
-  const res = await axios.post('/api/blogs', values);
+	const res = await axios
+		.post("/api/blogs", values)
+		.catch(err => console.log(err));
 
-  history.push('/blogs');
-  dispatch({ type: FETCH_BLOG, payload: res.data });
+	history.push("/blogs");
+	dispatch({ type: FETCH_BLOG, payload: res.data });
 };
 
 export const fetchBlogs = () => async dispatch => {
-  const res = await axios.get('/api/blogs');
+	const res = await axios.get("/api/blogs").catch(err => console.log(err));
 
-  dispatch({ type: FETCH_BLOGS, payload: res.data });
+	dispatch({ type: FETCH_BLOGS, payload: res.data });
 };
 
 export const fetchBlog = id => async dispatch => {
-  const res = await axios.get(`/api/blogs/${id}`);
+	const res = await axios
+		.get(`/api/blogs/${id}`)
+		.catch(err => console.log(err));
 
-  dispatch({ type: FETCH_BLOG, payload: res.data });
+	dispatch({ type: FETCH_BLOG, payload: res.data });
 };
---
+++ services/cache.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const promisify = require("util").promisify;
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.hget = promisify(client.hget);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function(options = {}) {
+	this.useCache = true;
+	// DO NOT USE EMPTY STRING AS KEY
+	this.hashKey = JSON.stringify(options.key || "");
+
+	return this;
+};
+
+mongoose.Query.prototype.exec = async function() {
+	if (!this.useCache) {
+		console.log("NO CACHE");
+		return exec.apply(this, arguments);
+	}
+
+	const key = JSON.stringify({
+		...this.getQuery(),
+		collection: this.mongooseCollection.name
+	});
+	console.log("key", key);
+	const cachedValue = await client
+		.hget(this.hashKey, key)
+		.catch(err => console.log(err));
+
+	if (cachedValue) {
+		const doc = JSON.parse(cachedValue);
+		console.log("RETURN FROM CACHE");
+		return Array.isArray(doc)
+			? doc.map(d => new this.model(d))
+			: new this.model(doc);
+	} else {
+		console.log("RETURN FROM MONGODB");
+		const result = await exec
+			.apply(this, arguments)
+			.catch(err => console.log(err));
+
+		client.hset(this.hashKey, key, JSON.stringify(result));
+		client.expire(this.hashKey, 15);
+
+		return result;
+	}
+};
+
+module.exports = {
+	clearHash(hashKey) {
+		client.del(JSON.stringify(hashKey));
+	}
+};
